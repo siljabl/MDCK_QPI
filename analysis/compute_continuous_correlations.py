corrected masking from previous commit

analysis/compute_continuous_correlations.py
@@ -53,7 +53,7 @@ except:
 config = json.load(open(f"{args.path}/config.txt"))
 
 fmin = config["fmin"]
-fmax = config["fmax"]-1 #-1 because PIV missing last frame
+fmax = config["fmax"]-1
 if args.fmax != None:
     fmax = args.fmax
 
@@ -108,6 +108,7 @@ PIV_height     = np.ma.zeros((fmax, xmax, xmax), dtype=np.float64) # not PIV, bu
 
 im_height = np.ma.copy(stack)
 
+
 # Fill arrays
 for frame in tqdm(range(fmax)):
 
@@ -128,7 +129,9 @@ for frame in tqdm(range(fmax)):
     frame_mean_height = np.ma.mean(stack[frame][stack[frame] > 0])
 
     # Probably better to coarse grain im (instead of just taking stack[x,y])
-    PIV_height[frame, x_tmp, y_tmp]           = stack[frame, x, y]
+    for i in range(len(x_tmp)):
+        PIV_height[frame, x_tmp[i], y_tmp[i]] = np.mean(stack[frame, x[i]-int(dx/2):x[i]+int(dx/2), y[i]-int(dx/2):y[i]+int(dx/2)])
+    #PIV_height[frame, x_tmp, y_tmp]           = stack[frame, x, y]
     PIV_height[frame][PIV_height[frame] > 0] -= frame_mean_height
     
     # subtrack mean
@@ -237,11 +240,9 @@ t_max = int(args.t_max * len(density))
 PIV_velocity = [PIV_velocity_x, PIV_velocity_y]
 
 # compute correlation and bin
-
-print(np.shape(PIV_velocity))
 C_t_vv = general_temporal_correlation(PIV_velocity, PIV_velocity, t_max=t_max)
-#C_t_hh = general_temporal_correlation(height,       height,       t_max=t_max)
-#C_t_hv = general_temporal_correlation(PIV_height,   PIV_velocity, t_max=t_max)
+C_t_hh = general_temporal_correlation(height,       height,       t_max=t_max)
+C_t_hv = general_temporal_correlation(PIV_height,   PIV_velocity, t_max=t_max)
 
 t_arr = np.arange(t_max) * frame_to_hour
 r_arr  = C_r_vv['r_bin']
@@ -253,9 +254,9 @@ r_arr  = C_r_vv['r_bin']
 ##################
 out_dict = {'C_t_vv': C_t_vv['C_norm'],
             'C_r_vv': C_r_vv['C_norm'],
-            #'C_t_hh': C_t_hh['C_norm'],
+            'C_t_hh': C_t_hh['C_norm'],
             'C_r_hh': C_r_hh['C_norm'],
-            #'C_t_hv': C_t_hv['C_norm'],
+            'C_t_hv': C_t_hv['C_norm'],
             'C_r_hv': C_r_hv['C_norm'],
             't_vv':   t_arr,
             'r_vv':   r_arr, 

plotting/plot_continuous_correlations.py
@@ -72,7 +72,7 @@ f_max = len(data['C_r_vv'])
 C_r_binned, density_bins = bin_by_density(data['C_r_vv'], density, bin_size=args.bin_size)
 C_t_binned, _            = bin_by_density(data['C_t_vv'], density, bin_size=args.bin_size)
 C_t_binned = np.ma.array(C_t_binned, mask=C_t_binned==0)
-print(data['C_t_vv'])
+
 
 # define colormap
 Nbins  = len(density_bins) - 1
@@ -108,8 +108,8 @@ fig.savefig(f"{args.in_path}/figs/autocorrelations_PIV.png", dpi=300, bbox_inche
 
 # bin correlations by density
 C_r_binned, density_bins = bin_by_density(data['C_r_hh'], density, bin_size=args.bin_size)
-#C_t_binned, _            = bin_by_density(data['C_t_hh'], density, bin_size=args.bin_size)
-#C_t_binned = np.ma.array(C_t_binned, mask=C_t_binned==0)
+C_t_binned, _            = bin_by_density(data['C_t_hh'], density, bin_size=args.bin_size)
+C_t_binned = np.ma.array(C_t_binned, mask=C_t_binned==0)
 
 r_hh = np.arange(len(C_r_binned[0])) * np.max(r_arr) / len(C_r_binned[0])
 
@@ -125,7 +125,7 @@ ax[1].hlines(0, 0, r_hh.max(),  linestyles="dashed", color="gray")
 
 # loop over density
 for i in range(Nbins):
-    #ax[0].plot(t_arr, C_t_binned[i], color=colors[i])
+    ax[0].plot(t_arr, C_t_binned[i], color=colors[i])
     ax[1].plot(r_hh,  C_r_binned[i], color=colors[i])
 
 
@@ -142,9 +142,9 @@ fig.savefig(f"{args.in_path}/figs/autocorrelations_height_continous.png", dpi=30
 
 # bin correlations by density
 C_r_binned, density_bins = bin_by_density(data['C_r_hv'], density, bin_size=args.bin_size)
-#C_t_binned, _            = bin_by_density(data['C_t_hv'], density, bin_size=args.bin_size)#
-#C_t_binned = np.ma.array(C_t_binned, mask=C_t_binned==0)
-
+C_t_binned, _            = bin_by_density(data['C_t_hv'], density, bin_size=args.bin_size)#
+C_t_binned = np.ma.array(C_t_binned, mask=C_t_binned==0)
+print(data['C_t_hv'])
 
 # define colormap
 Nbins  = len(density_bins) - 1
@@ -163,7 +163,7 @@ ax[1].hlines(0, 0, r_arr.max(), linestyles="dashed", color="gray")
 
 # loop over density
 for i in range(Nbins):
-    #ax[0].plot(t_arr, C_t_binned[i], color=colors[i])
+    ax[0].plot(t_arr, C_t_binned[i], color=colors[i])
     ax[1].plot(r_arr, C_r_binned[i], color=colors[i])
 
 